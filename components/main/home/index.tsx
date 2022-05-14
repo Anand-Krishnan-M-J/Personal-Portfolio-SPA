13783bd263647ce010a89c5cd273363143e590f7
AKMJ <anandkrish0646@gmail.com>
2022-05-15 00:43:53 +0530

    fix build errors

--- components/main/home/index.tsx
+++ components/main/home/index.tsx
@@ -9,18 +9,18 @@ export const Home = () => {
   return (
     <div className={classes.intro__container}>
       <div className={classes.intro__image__container}>
-        <Image priority src={image} />
+        <Image alt="Web Developer" priority src={image} />
       </div>
       <div className={classes.intro__content}>
         <h2 className={classes.intro__self}>{homeMessages.anand}</h2>
         <h1 className={classes.intro__profession}>{homeMessages.fullStackWebDeveloper}</h1>
         <div className={classes.intro__horizontalSeparation} />
-        <p className={classes.intro__description}>
-          I'm a software engineer, specializing in building simple to complex and
+        <p className={classes.intro__description}>{
+          `I'm a software engineer, specializing in building simple to complex and
           interactive web applications that run across platforms & devices.
           I am passionate about
           building excellent software that improves the lives of those
-          around me.
+          around me.`}
         </p>
 
       </div>
