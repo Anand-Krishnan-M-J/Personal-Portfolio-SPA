f9e4c4bf5cdc1b2ce3829397798eaba7bf03f2b0
AKMJ <anandkrish0646@gmail.com>
2022-07-22 01:32:58 +0530

    fix mobile style issues and make rotor nav defaul in mobile

--- components/layout/header/menu/index.tsx
+++ components/layout/header/menu/index.tsx
@@ -10,83 +10,86 @@ import MenuIcon from '@mui/icons-material/Menu';
 import classes from "./menu.module.scss";
 import { useDarkMode } from "../../../../hooks/useDarkMode";
 import { TabContext } from "../../../../pages";
+import { joinClass } from "../../../../helpers/utils";
 
 const rotorSize = 81;
-const xOffset = 28;
+const xOffset = 24;
 const yOffset = 20;
 const rotateOffset = 180;
 
 export const RotaryMenu = () => {
     const { tabValue, handleTabChange } = useContext(TabContext)
-    const {isDarkMode}= useDarkMode()
+    const { isDarkMode } = useDarkMode()
     const iconStyle = {
         color: "#2753d7",
         fontSize: "3rem",
-        backgroundColor:isDarkMode?"rgba(8, 8, 8, 1)":"#fff",
-        padding:"0.5rem",
-        borderRadius:"50%",
-        boxShadow:"0 0 0.5rem #2753d7"
+        backgroundColor: isDarkMode ? "rgba(8, 8, 8, 1)" : "#fff",
+        padding: "0.5rem",
+        borderRadius: "50%",
+        boxShadow: "0 0 0.5rem #2753d7"
     }
-   
+
     const icons = [
         {
-            name: "Home",
-            tabNumber:0,
-            IconComponent: <HomeIcon sx={iconStyle} />
-    
+            name: "Skills",
+            tabNumber: 3,
+            IconComponent: <PsychologyIcon sx={iconStyle} />
+
         },
         {
             name: "Portfolio",
-            tabNumber:1,
+            tabNumber: 1,
             IconComponent: <BusinessCenterIcon sx={iconStyle} />
-    
+
         },
         {
             name: "My Blogs",
-            tabNumber:2,
+            tabNumber: 2,
             IconComponent: <RssFeedIcon sx={iconStyle} />
-    
+
         },
+        
         {
-            name: "Skills",
-            tabNumber:3,
-            IconComponent: <PsychologyIcon sx={iconStyle} />
-    
+            name: "Home",
+            tabNumber: 0,
+            IconComponent: <HomeIcon sx={iconStyle} />
+
         },
         {
             name: "About",
-            tabNumber:4,
+            tabNumber: 4,
             IconComponent: <PersonIcon sx={iconStyle} />
-    
-    
+
+
         },
         {
             name: "Contact",
-            tabNumber:5,
+            tabNumber: 5,
             IconComponent: <ContactPhoneIcon sx={iconStyle} />
-    
+
         }
     ]
     const newI = icons.map((item, index) => {
         const angle = index * (360 / icons.length)
         const radianVal = angle * Math.PI / 180
         return {
-            tabNumber:item.tabNumber,
+            tabNumber: item.tabNumber,
             icon: item.IconComponent,
             IconComponent: item.IconComponent,
             xCordinate: rotorSize + rotorSize * Math.cos(radianVal),
             yCordinate: rotorSize + rotorSize * Math.sin(radianVal),
-    
+
         }
     })
-    
-
-
-
 
     const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(false);
     const [rotatebyAngle, setRotateByAngle] = useState(0);
+    const [is, setIs] = useState(isComponentVisible);
 
+
+    useEffect(() => {
+        setIs(isComponentVisible)
+    }, [isComponentVisible])
     useEffect(() => {
         if (!isComponentVisible) {
             setRotateByAngle(0);
@@ -95,8 +98,9 @@ export const RotaryMenu = () => {
 
     return (
         <div
-            className={isComponentVisible ? classes["mainContainer--open"] : classes["mainContainer--close"]}
+            className={is ? classes["mainContainer--open"] : classes["mainContainer--close"]}
             style={{
+                
                 width: "100%",
                 position: "fixed",
                 height: rotorSize * 2, display: "flex", justifyContent: "center"
@@ -107,24 +111,29 @@ export const RotaryMenu = () => {
                     transition: "transform 1s ease"
                 }}
                 ref={ref}
-                className={isComponentVisible ? classes['rotaryMenu--open'] :
+                className={is ? classes['rotaryMenu--open'] :
                     classes['rotaryMenu--close']}
                 onClick={() => {
                     setIsComponentVisible(true);
+                    setIs(true)
                 }}
             >
                 <div className={classes.rotoryMenu__container}>
-                    <div className={classes.menuIcon} onClick={() => {
+                    <div className={joinClass(classes.menuIcon,!isComponentVisible?classes.bounce:"")} onClick={() => {
                         window.navigator.vibrate(10);
-                        setRotateByAngle(rotatebyAngle + rotateOffset);
+                        setRotateByAngle(rotateOffset);
 
                     }} >
                         <MenuIcon sx={iconStyle} />
                     </div>
-                    {isComponentVisible && <div className={classes['rotaryMenu__icons__wrapper']}>
+                    {console.log(isComponentVisible, "is")}
+                    {is && <div className={classes['rotaryMenu__icons__wrapper']}>
                         {newI.map(item =>
-                            <div 
-                                onClick={()=>{handleTabChange(null as any,item.tabNumber)}}
+                            <div
+                                onClick={() => {
+                                    handleTabChange(null as any, item.tabNumber)
+                                    setIs(false);
+                                }}
                                 key={`rotaryIcon-${item.xCordinate}`}
                                 className={classes['rotaryMenu__icons']}
                                 style={{
