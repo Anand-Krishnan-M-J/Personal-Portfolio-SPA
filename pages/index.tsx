fe2a0f57596306c1ef60e98958ea29744b99723b
AKMJ <anandkrish0646@gmail.com>
2022-08-18 23:58:40 +0530

    fix animation bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,8 +79,9 @@ const Main = () => {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <TabContext.Provider value={{ tabValue, handleTabChange }}>
-        {!(isBlogsLoading || isProjectLoading) ?
+      {!(isBlogsLoading || isProjectLoading) ?
+        <TabContext.Provider value={{ tabValue, handleTabChange }}>
+
           <Layout variant="l1">
 
             <>
@@ -112,11 +113,11 @@ const Main = () => {
               </section>
             </>
 
-          </Layout> :
-          <Loading />
-        }
+          </Layout>
 
-      </TabContext.Provider>
+        </TabContext.Provider> :
+        <Loading />
+      }
     </div>
   )
 }
