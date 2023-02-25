9bd62e0c2593a646418b2696552cca2d6084ee8e
AKMJ <anandkrish0646@gmail.com>
2023-02-25 15:49:21 +0530

    update env and 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
-import Link from 'next/link'
 import styles from './404.module.scss'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <h1>Page not found ann mwonuseyy!!!</h1>
-      <h2>Link oke nokiyum kandum type cheyyande</h2>
+      <h1>Page not found</h1>
     </div>
   )
 }
