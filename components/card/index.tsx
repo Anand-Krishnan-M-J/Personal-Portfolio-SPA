084038355d66a1e8d76042cdd0dbaace888451dc
AKMJ <anandkrish0646@gmail.com>
2023-03-26 07:42:40 +0530

    Add project image and description in fallback mock

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography, Button, Grid } from "@mui/material"
+import Image from "next/image"
 import { useRouter } from 'next/router'
 import { TechUsed } from "../techUsed"
 import { CardItemProps } from "./card.type"
@@ -24,14 +25,20 @@ export const Card = ({ title, date, description, techStack, image, slug, endpoin
             }}>
             <Grid item xs={12} md={4} className={classes.card__container}
                 sx={{ margin: "1rem", display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}>
-                <img className={classes.card__image} src={image} alt={title} />
+                <Image
+                    layout="responsive"
+                    priority
+                    width={400}
+                    height={300}
+                    src={image}
+                    alt={title} />
                 <TechUsed techStack={techStack} />
             </Grid>
             <Grid item xs={12} md={6} justifyContent='flex-start'
                 sx={{ textAlign: 'left', margin: '1rem' }}>
                 <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
                     <Typography component="h2" sx={{ fontWeight: "600", fontSize: "large", marginRight: '1rem' }}>{title}</Typography>
-                    <Typography sx={{ color: "#2753d7", fontWeight: "600"}}>{date}</Typography>
+                    <Typography sx={{ color: "#2753d7", fontWeight: "600" }}>{date}</Typography>
                 </Box>
                 <Typography className={classes.description} sx={{ marginTop: "2rem", textOverflow: "ellipsis" }}>
                     {description}
